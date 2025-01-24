7fedda9e1c0e4a27129aeb516a3d21040d8bf133
Diana Ermolaeva <gavalonee@gmail.com>
2025-01-24 23:30:45 +0300

    first commit

---
+++ src/Task_8.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Task_8 {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Задание 8");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false); // только для чтения
+
+        StringBuilder taskText = new StringBuilder(" Кубы первых десяти положительных чисел: \n");
+
+        for (int i = 1; i < 11; i += 1){
+            int result = i * i * i;
+            taskText.append(i).append("^3 = ").append(result).append("\n");
+        }
+
+        textArea.setText(taskText.toString());
+        frame.add(textArea);
+        frame.setVisible(true);
+    }
+
+}
